8467f21b352ab8df9bbb9f5a41415149d5571527
Cédric Krier <ced@b2ck.com>
2010-11-01 23:02:45 +0100

    Prepare release 1.8.0

--- neso/version.py
+++ neso/version.py
@@ -1,6 +1,6 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 PACKAGE = "neso"
-VERSION = "1.7.0"
+VERSION = "1.8.0"
 LICENSE = "GPL-3"
 WEBSITE = "http://www.tryton.org/"
