8839d678a4bea6fe5e825ad73bfb38a3e0007fbf
Cédric Krier <ced@b2ck.com>
2015-12-27 18:31:00 +0100

    Add missing chardet as packages for py2exe
    chardet is a new dependency added to tryton by changeset a505295d6235
    
    issue5200
    review16911002
    (grafted from 9eeed65ee58eb732d0e65beb131f8590393022a9)
    
    --HG--
    branch : 3.8

--- setup.py
+++ setup.py
@@ -96,6 +96,7 @@ if os.name == 'nt':
                 'uuid',
                 'simpleeval',
                 'cached_property',
+                'chardet',
             ],
             'dll_excludes': ['dnsapi.dll', 'usp10.dll', 'iphlpapi.dll'],
             'excludes': ['Tkconstants', 'Tkinter', 'tcl'],
